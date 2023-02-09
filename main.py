6cf651e5175cd1166de970d3b96d13d279485fa1
Liam Gibbons <112109780+lgibbons12@users.noreply.github.com>
2023-02-09 00:40:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash 
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
-from pandas import json_normalize
 import json
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-#creating dataframe
-global df 
+fig,ax = plt.subplots(figsize=(6, 6))
+ax = sns.set_style(style="darkgrid")
+#To-Do
+#finish all statistical summaries
+#add all session data to the logout page
+#comment everything
+#commit everything
+#create READMe
+#create log of user interaction
+#push onto linode
+
+
 
 
 
@@ -29,6 +42,8 @@ class users(db.Model):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+
 
 @app.route("/")
 def home():
@@ -108,7 +123,7 @@ def stats():
 
 
                 session['in progress'] = True
-                print(session["in progress"])
+                session['stats-form'] = 0
                 return redirect(url_for("statsinprogress"))
             
             return render_template("stats.html")
@@ -117,7 +132,8 @@ def stats():
 
 @app.route("/statsinprogress", methods = ["POST", "GET"])
 def statsinprogress():
-    
+    means_list = []
+    medians_list = []
     if 'logged in' in session:
         if session['logged in'] == False:
             flash("You are not logged in. Please log in before using Stats Summarizer")
@@ -125,15 +141,81 @@ def statsinprogress():
         else:
             if 'in progress' in session:
                 if session['in progress'] == True:
-                    if request.method == "POST":
-                        with open("df.json", "r") as f:
+                    with open("df.json", "r") as f:
                             data = json.load(f)
-                        df = pd.read_json(data, orient="index")
-                        a = df.columns
+                    df = pd.read_json(data, orient="index")
+                    columns_list = []
+                    for i in range(len(df.columns)):
+                        columns_list.append(df.columns[i])
+                    if columns_list[0] == "Unnamed: 0":
+                        columns_list.remove("Unnamed: 0")
+                    
+                    cool = session['stats-form']
+                    
+                    if request.method == "POST":
+                        re_run = False
+                        if 'return' in request.form:
+                            
+                            session['stats-form'] = 0
+                            re_run = True
+                                
                         
-                        return render_template("statsinprogress.html", a=a)
+                        text_to_return = ""
+                        
+                        for column in df:
+                            if df[column].dtype != 'int64' and df[column].dtype != 'float64':
+                                columns_list.remove(column)
+                        if session['stats-form'] == 0 and re_run == False:
+                            output = request.form['firstselect']
+
+                            if output == "Quick Summary":
+                                text_to_return = f"This dataset has {len(columns_list)} numerical columns, all named: {columns_list}"
+                                
+                                for column in columns_list:
+                                    means_list.append(df[column].mean())
+                                
+                                for column in columns_list:
+                                    medians_list.append(df[column].median())
+                            elif output == "Relationship":
+                                session['stats-form'] = 2
+                            elif output == "Graph":
+                                session['stats-form'] = 3
+                        
+                        
+                        
+                        elif session["stats-form"] == 2 or session['stats-form'] == 3:
+                            print("h1")
+                            if 'relselect' in request.form:
+                                relselect = request.form.getlist('relselect')
+                            else:
+                                relselect = ["", ""]
+                            
+                            if session['stats-form'] == 2:
+                                correlation = df[relselect[0]].corr(df[relselect[1]])
+                                correlation = round(correlation, 3)
+                                if correlation > 0:
+                                    sign = "positive, which means that as one goes up so does the other. "
+                                else:
+                                    sign = "negative, which means that as one goes up, the other goes down. "
+                                if abs(correlation) > 0.5:
+                                    magnitude = f"The significance of the relationship between {relselect[0]} and {relselect[1]} is high, with a correlation of {correlation}, which means they are closely associated"
+                                else:
+                                    magnitude = f"The significance of the relationship between {relselect[0]} and {relselect[1]} is low, with a correlation of {correlation}, which means they are not closely associated"
+                                text_to_return = f"The correlation test shows the the correlation between {relselect[0]} and {relselect[1]} is {sign} {magnitude}"
+                            else:
+                                session['plotx'] = relselect[0]
+                                session['ploty'] = relselect[1]
+                                return redirect(url_for("graph"))
+                            
+                        cool = session['stats-form']
+                        
+                        
+
+                        
+                        return render_template("statsinprogress.html", means = means_list, medians = medians_list, url='/static/new_plot.png', show=cool, list = columns_list, text=text_to_return)
                     else:
-                        return render_template("statsinprogress.html")
+                        session['stats-form'] = 0
+                        return render_template("statsinprogress.html", means = means_list, medians = medians_list, show=cool, list=columns_list)
                 else:
                     return render_template("statsinprogress.html")
             else:
@@ -142,6 +224,30 @@ def statsinprogress():
     else:
         flash("You are not logged in. Please log in before using Stats Summarizer")
         return redirect(url_for("login"))
+
+@app.route('/get-plot', methods = ['POST', 'GET'])
+def get_plot():
+    if request.method == "POST":
+        if 'return' in request.form:
+            session['stats-form'] = 0
+            return redirect(url_for("statsinprogress"))
+
+        if 'relselect' in request.form:
+            relselect = request.form.getlist('relselect')
+        else:
+            relselect = ["", ""]
+        with open("df.json", "r") as f:
+                data = json.load(f)
+        df = pd.read_json(data, orient="index")
+        plt.scatter(df[relselect[0]], df[relselect[1]])
+        plt.title(f"{relselect[1]} vs. {relselect[0]}")
+        plt.xlabel(relselect[0])
+        plt.ylabel(relselect[1])
+        plt.savefig('static/my_plot.png')
+        return render_template('graph.html', plot_url = "static/my_plot.png")
+    else:
+        flash("Please Go Through Stats In Progress To Get To The Graph")
+        return redirect(url_for("stats"))
 
 
 @app.route("/logout")
